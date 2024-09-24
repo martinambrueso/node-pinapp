1a6c245b34882cd60082cd91de6d8afbd586bdb1
ma_i02114 <m.ambrueso@redlink.com.ar>
2024-09-24 13:06:51 -0300

    test

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  console.log("Hello World")
+  console.log("Hello World 2")
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
