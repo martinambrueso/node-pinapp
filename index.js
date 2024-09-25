5457e714730cce3a99de4a4c565deb87f609f0fa
Martin Ambrueso <martinambrueso@hotmail.com>
2024-09-24 22:02:05 -0300

    cleanup project

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World para todos!");
+  res.send("Hello World para todos 2!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
