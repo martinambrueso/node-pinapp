a3908840b2af7e32eed7678b1b25058e07e53fc7
Martin Ambrueso <martinambrueso@hotmail.com>
2024-09-26 16:17:22 -0300

    test

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send({
+    message: "Hello World",
+    status: 200,
+  });
 });
 
 app.listen(port, () => {
